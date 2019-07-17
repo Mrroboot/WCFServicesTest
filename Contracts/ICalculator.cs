33dd54846da2d553ff34c69693ec42aea06a45fd
yxwang <756582498@qq.com>
2019-07-17 16:07:48 +0800

    commit message

---
+++ Contracts/ICalculator.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace Contracts
+{
+    [ServiceContract(Name="CalculatorService",Namespace="http://www.artech.com/")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double x, double y);
+
+        [OperationContract]
+        double Subtract(double x, double y);
+
+        [OperationContract]
+        double Multiply(double x, double y);
+
+        [OperationContract]
+        double Divide(double x, double y);
+
+    }
+}
